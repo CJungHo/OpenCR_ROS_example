f98d9655aec015f4a270cdb7155f2e74af8e6a19
CJungHo <91372509+CJungHo@users.noreply.github.com>
2021-09-25 20:19:03 +0900

    Add files via upload

---
+++ arduino_custom_msg/custom_msg_example/custom_msg_example.ino
@@ -0,0 +1,26 @@
+#include <ros.h>
+#include <test_msgs.h>
+
+ros::NodeHandle nh;
+
+test_msgs::test_msgs msg;
+ros::Publisher pub("custom_msg", &msg);
+int count = 0;
+
+void setup() {
+  nh.initNode();
+  nh.advertise(pub);
+}
+
+void loop() {
+  uint32_t t = millis();
+  if((t - tTime) >= 1000)
+  {
+    msg.id.data = 1;
+    msg.data.data = count;
+    data_pub.publish(&msg);
+    count++;
+    tTime = t;
+  }
+  nh.spinOnce();
+}
