066df9b6e032474521a0be92352e4184343816f0
CJungHo <91372509+CJungHo@users.noreply.github.com>
2021-09-25 19:56:18 +0900

    Add files via upload

---
+++ dynamixel_control/dynamixel_control.ino
@@ -0,0 +1,24 @@
+#include "Motor_driver.h"
+
+MotorDriver motor_driver;
+
+void setup() {
+  Serial.begin(9600);
+  motor_driver.init();
+}
+
+void loop() {
+  //속도 제어
+  motor_driver.VelControl(1, 100); // id, vel(-255 ~ 255)
+
+  //위치 제어
+  motor_driver.PosControl(1, 2048); // id, pos(0~4096)
+
+  //모터 위치값 읽기
+  static int32_t pos;
+  motor_driver.readEncoder(pos);
+  Serial.print("pos : ");
+  Serial.println(pos);
+}
+
+
---
+++ publisher_example/publisher_example.ino
@@ -0,0 +1,28 @@
+#include <ros.h>
+#include <std_msgs/Int16.h>
+
+ros::NodeHandle nh;
+
+std_msgs::Int16 value;
+ros::Publisher data_pub("topic", &value);
+static uint32_t tTime = 0;
+int count = 0;
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(data_pub);
+}
+
+void loop()
+{
+  uint32_t t = millis();
+  if((t - tTime) >= 1000)
+  {
+    value.data = count;
+    data_pub.publish(&value);
+    count++;
+    tTime = t;
+  }
+  nh.spinOnce();
+}
